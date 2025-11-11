d431105b04823b254deb98faceb62041427e368f
senai_25_26 <jhmsg45@gmail.com>
2025-11-11 15:59:55 -0300

    aa

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, flash
 from flask import render_template, redirect, request, url_for
 from sqlalchemy.exc import SQLAlchemyError
-from models import local_secao, Ator, Avaliacao, Diretor, Filme, Genero, Usuario
+from models import local_secao, Ator, Avaliacao, Filme, Usuario
 from sqlalchemy import select
 
 app = Flask(__name__)
@@ -14,53 +14,21 @@ def index():
 
 @app.route('/pag_inicial')
 def home():
-    return render_template('Pag_inicio.html')
-
+    return render_template('Inicio.html')
 
 @app.route('/filmes')
-def get_filmes():
+def listar_filmes():
     db_session = local_secao()
     try:
         sql_filmes = select(Filme)
-        resultado = db_session.execute(sql_filmes).scalars()
-        return render_template('Pag_inicio.html', var_filmes=resultado)
+        resultado = db_session.execute(sql_filmes)
+        return render_template('Inicio.html', var_filmes=resultado)
     except SQLAlchemyError as e:
         print(f"Erro na base de dados: {e}")
     except Exception as ex:
-        print(f'Ocorreu um erro: {ex}')
+        print(f'Ocorreu um erro ao consultar receitas: {ex}')
     finally:
         db_session.close()
-
-@app.route('/cadastrar_filme', methods=['GET', 'POST'])
-def post_filme():
-    db_session = local_secao()
-    if request.method == 'POST':
-        if not request.form('titulo'):
-            flash("preencha o titulo", "error")
-        if not request.form('duracao'):
-            flash("preencha o duração", "error")
-        if not request.form('descricao'):
-            flash("preencha o descrição", "error")
-        if not request.form('trailer'):
-            flash("preencha o trailer", "error")
-        if not request.form('lancamento'):
-            flash("preencha o lançamento", "error")
-        if not request.form('genero'):
-            flash("preencha o genero", "error")
-        if not request.form('diretor'):
-            flash("preencha o diretor", "error")
-        dados_filme = Filme(titulo=request.form['titulo'],tempo_duracao_min=request.form['duracao'],descricao=request.form['descricao'], trailer=request.form['trailer'],data_lancamento=request.form['lanamento'], id_genero=request.form['genero'],id_diretor=request.form['diretor'])
-        try:
-            db_session.add(dados_filme)
-            db_session.commit()
-            flash("Cadastrado com sucesso!", "success")
-            return redirect(url_for('/pag_inicial'))
-        except SQLAlchemyError as e:
-            print(f"Erro ao cadastrar pessoa: {e}")
-            db_session.rollback()
-        finally:
-            db_session.close()
-    return render_template('Cadastrar.html')
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -13,59 +13,51 @@ local_secao = sessionmaker(bind=engine)
 Base = declarative_base()
 
 # Base
-class Ator(Base):
-    __tablename__ = 'autores'
-    id_ator = Column(Integer, primary_key=True)
-    nome_ator = Column(String(150), nullable=False)
-    biografia = Column(String(255), nullable=False)
-    papel_interpretado = Column(String(255), nullable=False)
-    def __repr__(self):
-        return (f'<Ator {self.nome_ator}, biografia: {self.biografia},papel_interpretado: {self.papel_interpretado} >')
-
-class Genero(Base):
-    __tablename__ = 'generos'
-    id_genero = Column(Integer, primary_key=True)
-    nome_genero = Column(String(50), nullable=False)
-    descricao = Column(String(255), nullable=False)
-    def __repr__(self):
-        return (f'<Genero {self.nome_genero}, Descricao: {self.descricao}>')
-
-class Diretor(Base):
-    __tablename__ = 'diretores'
-    id_diretor = Column(Integer, primary_key=True)
-    nome_diretor = Column(String(100), nullable=False)
-    biografia = Column(String(255), nullable=False)
-    def __repr__(self):
-        return (f'<Diretor {self.nome_diretor}, Biografia: {self.biografia}>')
-
-
 class Filme(Base):
     __tablename__ = 'filmes'
     id_filme = Column(Integer, primary_key=True)
-    titulo = Column(String(80), nullable=False)
-    tempo_duracao_min = Column(Integer)
-    descricao = Column(String(255), nullable=False)
+    titulo = Column(String(150), nullable=False)
+    tempo_duracao_min = Column(String(50), nullable=False)
+    descricao = Column(String, nullable=False)
     trailer = Column(String(255), nullable=False)
-    data_lancamento = Column(String(255), nullable=False)
-    id_genero = Column(Integer, ForeignKey('generos.id_genero'))
-    id_diretor = Column(Integer, ForeignKey('diretores.id_diretor'))
+    data_lancamento = Column(String, nullable=False)
+    genero_filme = Column(String(50), nullable=False)
+    filme_diretor = Column(Integer, ForeignKey('diretores.id_diretor'), nullable=False)
+    filme_ator = Column(Integer, ForeignKey('atores.id_ator'), nullable=False)
     def __repr__(self):
-        return (f'<Filme {self.titulo}, Tempo: {self.tempo_duracao_min}, Descrição: {self.descricao}, Trailer: {self.trailer}, Data lançamento: {self.data_lancamento}, Genero: {self.id_genero}, Diretor: {self.id_filme}>')
+        return (f'<Filme {self.titulo}, Tempo: {self.tempo_duracao_min}, Descricao: {self.descricao}, Trailer: {self.trailer}, '
+                f'data_lancamento: {self.data_lancamento}, genero: {self.genero_filme}, filme do diretor: {self.filme_diretor},'
+                f'filme do ator: {self.filme_ator}>')
 
 class Avaliacao(Base):
     __tablename__ = 'avaliacoes'
     id_avaliacao = Column(Integer, primary_key=True)
-    critica = Column(String(255), nullable=False)
-    id_filme = Column(Integer, ForeignKey('filmes.id_filme'))
-    id_usuario = Column(Integer, ForeignKey('usuarios.id_usuario'))
+    nota = Column(String(5), nullable=False)
+    critica = Column(String, nullable=False)
     def __repr__(self):
-        return (f'<Avaliacao {self.critica}, Filme: {self.id_filme}, Usuario: {self.id_usuario}>')
+        return f'<Avaliacao: {self.nota} critica: {self.critica}>'
 
 class Usuario(Base):
     __tablename__ = 'usuarios'
     id_usuario = Column(Integer, primary_key=True)
-    nome = Column(String(255), nullable=False)
+    nome_usuario = Column(String(255), nullable=False)
     email = Column(String(255), nullable=False)
     senha = Column(String(255), nullable=False)
     def __repr__(self):
-        return (f'<Usuario {self.nome}, Email: {self.email}, Senha: {self.senha}>')
+        return f'<Usuario: {self.nome_usuario}, Email: {self.email}, Senha: {self.senha}>'
+
+class Ator(Base):
+    __tablename__ = 'atores'
+    id_ator = Column(Integer, primary_key=True)
+    nome_ator = Column(String(150), nullable=False)
+    def __repr__(self):
+        return f'<Ator: {self.nome_ator}>'
+
+class Diretor(Base):
+    __tablename__ = 'diretors'
+    id_diretor = Column(Integer, primary_key=True)
+    nome_diretor = Column(String(255), nullable=False)
+    def __repr__(self):
+        return f'<Diretor: {self.nome_diretor}>'
+
+
